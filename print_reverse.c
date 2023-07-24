201aa192a0ce37f2b10995c91d408cbe57979034
eoasante98 <eo.asante3@gmail.com>
2023-07-24 11:52:04 -0700

    updating print_reverse.c

--- print_reverse.c
+++ print_reverse.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reverse - ...
+ * pr_rev - reverses a string
  * @arch: reversed string
  * Return: reversed [string]
  */
@@ -8,3 +8,23 @@ char *pr_rev(va_list arch)
 {
 	char *back;
 	char *str;
+	int a = 0, len, b;
+
+	str = va_arg(arch, char *);
+	if (str == NULL)
+		str = ")llun(";
+
+	len = _strlen(str);
+
+	back = malloc(sizeof(char) * (len + 1));
+	if (back == NULL)
+		return (NULL);
+
+	for (b = (len - 1); b >= 0; b--)
+	{
+		back[a++] = str[b];
+	}
+	back[a] = '\0';
+
+	return (back);
+}
